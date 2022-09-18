8bd53437496435ae0e1c9b1f3c70eb2de2569dbf
oscarty01 <whiteoscar01@gmail.com>
2022-09-18 16:12:44 +0800

    added broswer detection

--- JavScript/script.js
+++ JavScript/script.js
@@ -8,4 +8,25 @@ function close_menu(){
     show.style.right = "-200px";
 }
 
-console.log("Script loaded")
+let userAgent = navigator.userAgent;
+let browser;
+
+if(userAgent.match(/edg/i)){
+    browser = "edge";
+}else if(userAgent.match(/firefox|fxios/i)){
+    browser = "firefox";
+}else if(userAgent.match(/opr\//i)){
+    browser = "opera";
+}else if(userAgent.match(/chrome|chromium|crios/i)){
+    browser = "chrome";
+}else if(userAgent.match(/safari/i)){
+    browser = "safari";
+}else{
+    alert("Other browser");
+}
+
+const browser_logos = document.querySelector(`.browser_logos .${browser}`);
+
+if(browser_logos){
+    browser_logos.style.opacity = "1";
+}
