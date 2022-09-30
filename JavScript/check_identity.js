48fbc09747b7b8974f836e45712b8d7d3036cfeb
oscarty01 <whiteoscar01@gmail.com>
2022-09-30 19:13:15 +0800

    new

--- JavScript/check_identity.js
+++ JavScript/check_identity.js
@@ -1,15 +1,19 @@
 var pwd = "Ds"
+var link = "https://docs.google.com/document/d/1ukBMDM4f46yrtIGQd_oDZg5mYfK5gLFtKfk_CUoJ_kE/edit?usp=sharing"
 
 function check_identity(){
     var user = document.getElementById("user_identification");
+    var o_link = document.querySelector(".password")
+    //console.log(o_link)
     //console.log(pwd)
     //console.log(user.value)
     if(user.value == pwd){
-        console.log("yes")
+        console.log("Correct pwd")
+        o_link.setAttribute("action",link)
     }
     else{
         window.alert("Please enter the correct credential")
-        console.log("no")
+        console.log("Wrong pwd")
         return false
     }
 }
